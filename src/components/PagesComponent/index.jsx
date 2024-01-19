d48fc143e626f3dc3db53b681daa4572cfd7828a
IsequielNascimento <isequielnascimento@gmail.com>
2024-01-19 09:48:12 -0300

    feat: PagesComponent HTML Conclusion

---
+++ src/components/PagesComponent/index.jsx
@@ -0,0 +1,34 @@
+export default function PagesComponent() {
+
+    return (
+        <section>
+            <h3>
+                Páginas
+            </h3>
+            <hr />
+            <div>
+                <button>
+                    Home
+                </button>
+                <button>
+                    Produtos
+                </button>
+                <button>
+                    Categorias
+                </button>
+                <button>
+                    Meus Pedidos
+                </button>
+            </div>
+            <hr />
+            <span>
+                <button>
+                    Cadastre-se
+                </button>
+                <button>
+                    Entrar
+                </button>
+            </span>
+        </section>
+    )
+}
--- src/components/Screens/Home/index.jsx
+++ src/components/Screens/Home/index.jsx
@@ -1,7 +1,9 @@
 import Header from "../../Header";
+import PagesComponent from "../../PagesComponent";
 
 export default function Home() {
     return (
-        <Header/>
+        // <Header/>
+        <PagesComponent/>
     )
 }
