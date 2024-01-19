9dfd0f44af77e8a80d2eb2beadbf98230073f612
IsequielNascimento <isequielnascimento@gmail.com>
2024-01-19 10:19:24 -0300

    feat: MyCart HTML Conclusion

--- src/components/Screens/Home/index.jsx
+++ src/components/Screens/Home/index.jsx
@@ -1,9 +1,12 @@
 import Header from "../../Header";
+import MyCart from "../../MyCart";
 import PagesComponent from "../../PagesComponent";
 
 export default function Home() {
     return (
         // <Header/>
-        <PagesComponent/>
+        // <PagesComponent/>
+        <MyCart/>
+
     )
 }
